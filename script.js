b6742b7f30dd87169eee958e31321461bcdd78a6
ByiringiroUrban <151029116+ByiringiroUrban@users.noreply.github.com>
2024-11-18 19:39:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript for Interactive Buttons
+
+document.querySelector('.btn-login').addEventListener('click', () => {
+    alert('Login feature coming soon!');
+});
+
+document.querySelector('.btn-signup').addEventListener('click', () => {
+    alert('Sign Up feature coming soon!');
+});
+
+document.querySelector('.btn-join').addEventListener('click', () => {
+    alert('Join Now feature coming soon!');
+});
+
+
+// Handle Join Now button click for Welcome Bonus section
+document.querySelector('.welcome-bonus .btn-join').addEventListener('click', () => {
+    alert('Redirecting to the signup page...');
+});
